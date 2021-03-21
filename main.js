0f3f6a6207c43aeb2e0908173cbd78d5b0717816
Daniel <danielarturo.marquez@gmail.com>
2021-03-21 11:36:07 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,110 @@
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
+
+const loginCheck = (user) => {
+  if (user) {
+    loggedInLinks.forEach((link) => (link.style.display = "block"));
+    loggedOutLinks.forEach((link) => (link.style.display = "none"));
+  } else {
+    loggedInLinks.forEach((link) => (link.style.display = "none"));
+    loggedOutLinks.forEach((link) => (link.style.display = "block"));
+  }
+};
+// Sign Up
+const signupForm = document.querySelector("#signup-form");
+
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector("#signup-email").value;
+  const password = document.querySelector("#signup-password").value;
+
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Clear form
+      signupForm.reset();
+
+      // Close Modal
+      $("#signUpModal").modal("hide");
+
+      console.log("sign up");
+    });
+});
+
+// Sign In
+const signinForm = document.querySelector("#login-form");
+
+signinForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector("#login-email").value;
+  const password = document.querySelector("#login-password").value;
+
+  auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
+    // Clear form
+    signinForm.reset();
+
+    // Close Modal
+    $("#signInModal").modal("hide");
+  });
+});
+
+// logout
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {});
+});
+
+// Google Login
+const googleButton = document.querySelector("#googleLogin");
+googleButton.addEventListener("click", (e) => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth
+    .signInWithPopup(provider)
+    .then((result) => {
+      signinForm.reset();
+      $("#signInModal").modal("hide");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//Posts
+const postList = document.querySelector(".posts");
+const setupPosts = (data) => {
+  if (data.length) {
+    let html = "";
+    data.forEach((doc) => {
+      const post = doc.data();
+      const li = `
+        <li class="list-group-item list-group-item-action">
+          <h5>${post.title}</h5>
+          <p>${post.description}</p>
+        </li>
+      `;
+      html += li;
+    });
+    postList.innerHTML = html;
+  } else {
+    postList.innerHTML = '<h4 class="text-center">Login to See Posts</h4>';
+  }
+};
+
+// Events
+// listar datos para usuarios autenticados
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    fs.collection("posts")
+      .get()
+      .then((snapshot) => {
+        setupPosts(snapshot.docs);
+        loginCheck(user);
+      });
+  } else {
+    setupPosts([]);
+    loginCheck(user);
+  }
+});
